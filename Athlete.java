2185e3a2768025f63ff5fc9cf3ba050411a73b84
skya1234 <sky.aniszewski@gmail.com>
2024-02-17 15:09:39 -0800

    changes 1

---
+++ Athlete.java
@@ -0,0 +1,11 @@
+public class Athlete {
+    
+    private double time100m; // looking for 100m value
+    private String name; 
+    private String country;
+    public Athlete(String n, String nationality, double result) {
+        name = n;
+        country  = nationality;
+        result = time100m;
+    }
+}
